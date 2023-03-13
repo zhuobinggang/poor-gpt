e6f24db1d4c46c0130e8906920828000e8449003
taku <zhuobinggang@gmail.com>
2023-03-13 15:10:40 +0900

    小更新

--- relation_extract/t5_coref.py
+++ relation_extract/t5_coref.py
@@ -17,6 +17,8 @@ def create_model(learning_rate = 2e-5, size = 'small', max_token_size = 512, t5 
         res.t5 = T5ForConditionalGeneration.from_pretrained(f"google/flan-t5-{size}")
     else:
         res.t5 = t5
+    res.max_token_size = max_token_size
+    res.size = size
     res.toker = T5Tokenizer.from_pretrained(f"google/flan-t5-{size}", truncation_side= 'left', model_max_length = max_token_size)
     # res.t5 = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
     # res.toker = T5Tokenizer.from_pretrained("google/flan-t5-base")
@@ -26,10 +28,10 @@ def create_model(learning_rate = 2e-5, size = 'small', max_token_size = 512, t5 
     return res
 
 def save_model(m, name):
-    torch.save(m.t5, f'./check_points/{name}.tch')
+    torch.save(m.t5, f'./check_points/{name}_size{m.size}_maxtoken{m.max_token_size}.tch')
 
 def load_model(name, size, max_token_size, learning_rate = 2e-5):
-    t5 = torch.load(f'./check_points/{name}.tch')
+    t5 = torch.load(f'./check_points/{name}_size{size}_maxtoken{max_token_size}.tch')
     res = create_model(learning_rate, size, max_token_size, t5)
     return res
 
